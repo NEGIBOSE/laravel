917299ed99eb1f1af63efa0fbaddf40f09ce32ac
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-01-31 00:26:43 +0900

    バリデーション解決せず

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
 ->name('tweet.index');
 
 Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
