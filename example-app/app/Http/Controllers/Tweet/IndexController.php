9cef88d4656efdfabce60ac8c34a461be9c191d2
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-02-05 14:06:56 +0900

    p87	,省略なし

--- example-app/app/Http/Controllers/Tweet/IndexController.php
+++ example-app/app/Http/Controllers/Tweet/IndexController.php
@@ -15,13 +15,13 @@ class IndexController extends Controller
     public function __invoke(Request $request)
     {
         $tweetId = (int) $request->route('tweetId');
-        $tweet = Tweet::where('id', $tweetId)->first0rFail();
-        // if (is_null($tweet)) {
-        //     throw new NotFoundHttpException('存在しないつぶやきです');
-        // }
+        $tweet = Tweet::where('id', $tweetId)->first();
+        if (is_null($tweet)) {
+            throw new NotFoundHttpException('存在しないつぶやきです');
+        }
         dd($tweetId);
-        // $tweets = Tweet::orderBy('created_at', 'DESC')->get();
-        // return view('tweet.index')
-        //     ->with('tweets', $tweets);
+        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        return view('tweet.index')
+            ->with('tweets', $tweets);
     }
 }
