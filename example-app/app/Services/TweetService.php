a36b89c8114872fc569a47df6bda8108d53513d2
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-02-17 10:10:11 +0900

    p236,22

--- example-app/app/Services/TweetService.php
+++ example-app/app/Services/TweetService.php
@@ -3,6 +3,10 @@
 namespace App\Services;
 
 use App\Models\Tweet;
+use Carbon\Carbon;
+use App\Models\Image;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class TweetService
 {
@@ -20,5 +24,22 @@ class TweetService
         }
 
         return $tweet->user_id === $userId;
+    }
+
+    public function saveTweet(int $userId, string $content, array $images)
+    {
+        DB:transaction(function () use ($userId, $content, $images) {
+            $tweet = new Tweet;
+            $tweet->user_id = $userId;
+            $tweet->content = $content;
+            $tweet->save();
+            foreach ($images as $images) {
+                Storage::putFile('public/images', $images);
+                $imageModels = new Image();
+                $imageModels->name = $image->hashName();
+                $imageModels->save();
+                $tweet->images()->attach($imageModels->id);
+            }
+        });
     }
 }
