116b28d83cfd5bb536835088f95ed9eeb47896f6
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-02-18 23:14:21 +0900

    p309,48

--- example-app/resources/views/tweet/images.blade.php
+++ example-app/resources/views/tweet/images.blade.php
@@ -8,8 +8,8 @@
         @foreach($images as $images)
         <div class="w-1/6 px-2 mt-5">
             <div class="bg-gray-400">
-                <a @click="$dispatch('img-modal', { imgModalSrc:'{{ asset('storage/images/' . $image->name) }}' )" class="cursor-pointer">
-                    <img src="{{ asset('storage/images/' . $image->name) }}" alt="{{ $image->name }}" class="object-fit w-full">
+                <a @click="$dispatch('img-modal', { imgModalSrc: '{{ image_url($image->name) }}' )" class="cursor-pointer">
+                    <img src="{{ image_url($image->name) }}" alt="{{ $image->name }}" class="object-fit w-full">
                 </a>
             </div>
         </div>
