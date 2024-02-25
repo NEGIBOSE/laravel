0be86fe779a68a3b718b8a427ceeb2f01d7ca6cf
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-02-26 00:27:17 +0900

    p126

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -8,22 +8,24 @@
     </head>
     <body>
         <h1>つぶやきアプリ</h1>
-        <div>
-            <p>投稿フォーム</p>
-            @if (session('feedback.success'))
-                <p style="color: green">{{ session('feedback.success')}}</p>
-            @endif
-            <form action="{{ route('tweet.create') }}" method="post">
-                @csrf
-                <label for="tweet-content">つぶやき</label>
-                <span>140文字までです</span>
-                <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
-                <button type="submit">投稿</button>
-                @error('tweet')
-                    <p style="color: red;">{{ $message }}</p>
-                @enderror
-            </form>
-        </div>
+        @auth
+            <div>
+                <p>投稿フォーム</p>
+                @if (session('feedback.success'))
+                    <p style="color: green">{{ session('feedback.success')}}</p>
+                @endif
+                <form action="{{ route('tweet.create') }}" method="post">
+                    @csrf
+                    <label for="tweet-content">つぶやき</label>
+                    <span>140文字までです</span>
+                    <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
+                    <button type="submit">投稿</button>
+                    @error('tweet')
+                        <p style="color: red;">{{ $message }}</p>
+                    @enderror
+                </form>
+            </div>
+        @endauth
         @foreach($tweets as $tweet)
             <details>
                 <summary>{{ $tweet->content }}</summary>
