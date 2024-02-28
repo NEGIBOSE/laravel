a4ccf9656db5adc6c328cbdcab87e38880f251a3
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-02-28 23:14:15 +0900

    chapter3途中

---
+++ resources/views/components/tweet/form/post.blade.php
@@ -0,0 +1,18 @@
+@auth
+<div class="p-4">
+    <p>投稿フォーム</p>
+    @if (session('feedback.success'))
+        <p style="color: green">{{ session('feedback.success')}}</p>
+    @endif
+    <form action="{{ route('tweet.create') }}" method="post">
+        @csrf
+        <label for="tweet-content">つぶやき</label>
+        <span>140文字までです</span>
+        <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
+        <button type="submit">投稿</button>
+        @error('tweet')
+            <p style="color: red;">{{ $message }}</p>
+        @enderror
+    </form>
+</div>
+@endauth
--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -4,12 +4,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+        <script src="{{ mix('/js/app.css') }}"></script>
         <title>つぶやきアプリ</title>
     </head>
     <body>
         <h1>つぶやきアプリ</h1>
         @auth
-        <div>
+        <div class="p-4">
             <p>投稿フォーム</p>
             @if (session('feedback.success'))
                 <p style="color: green">{{ session('feedback.success')}}</p>
